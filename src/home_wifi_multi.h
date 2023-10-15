71b45f79fec94ccd3433ea199c4fab5214046d9b
amit <itexperts.hire@gmail.com>
2023-10-15 09:09:40 -0700

    change

--- src/home_wifi_multi.h
+++ src/home_wifi_multi.h
@@ -1,7 +1,7 @@
-    #define COMMON_WIFI_PWD "*****"
+    #define COMMON_WIFI_PWD "81249022"
 
-  #define SSID1 "******"
-  #define PWD1 "*******"
+  #define SSID1 "Elly"
+  #define PWD1 "samr"
   #define SSID2 "*****"
   #define PWD2 "*****"
   #define SSID3 "*****"
